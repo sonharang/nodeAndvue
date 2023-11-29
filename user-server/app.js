d89a63900369beb653f7f1187b000ac3127d0b12
sonharang <bhson3@naver.com>
2023-11-29 17:28:20 +0900

    11/29 5:28

--- user-server/app.js
+++ user-server/app.js
@@ -15,30 +15,30 @@ app.listen(3001,()=>{
 });
 
 //전체조회
-app.get('/t_users', async(req, res)=>{
+app.get('/users', async(req, res)=>{ // /뒤에는 그냥 주소값임 db랑 상관없다.
     let list = await mysql.query('userList');
     res.send(list);
 });
 //단건조회
-app.get('/t_users/:user_no', async(req,res)=>{
+app.get('/users/:user_no', async(req,res)=>{
     let data = req.params.user_no;
     let result = await mysql.query('userInfo', data);
     res.send(result[0]);
 });
 //등록
-app.post('/t_users', async (req, res)=>{
+app.post('/users', async (req, res)=>{
     let data = req.body.param;
     let result = await mysql.query('userInsert',data);
     res.send(result);
 });
 //수정
-app.put('/t_users/:user_no', async (req, res)=>{
+app.put('/users/:user_no', async (req, res)=>{
     let datas = [req.body.param, req.params.user_no];
     let result = await mysql.query('userUpdate',datas);
     res.send(result);
 });
 //삭제
-app.delete('/t_users/:user_no', async(req, res)=>{
+app.delete('/users/:user_no', async(req, res)=>{
     let data = req.params.user_no;
     let result = await mysql.query('userDelete',data);
     res.send(result);

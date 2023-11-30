e47948e8bfa78c666d409b7b95a4d951a443bb6b
sonharang <bhson3@naver.com>
2023-11-30 12:35:12 +0900

    11/30 12:25

--- vue/vue-test/src/App.vue
+++ vue/vue-test/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <VueComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import VueComponent from './components/VueComponent.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    VueComponent
   }
 }
 </script>
---
+++ vue/vue-test/src/components/VueComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <label><input type="text" v-model="msg"></label>
+        <p>{{msg}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'VueComponent',
+    data(){
+        return{
+            msg:'Hmm..'
+        }
+    }
+}
+</script>
+<style>
+    p{
+        background-color: rgb(15, 237, 119);
+    }
+</style>

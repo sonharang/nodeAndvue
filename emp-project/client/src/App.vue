21049abe3e5f7d9c6380ca7574d66cc11451567b
sonharang <bhson3@naver.com>
2023-12-07 10:04:23 +0900

    12/7 10:04

---
+++ emp-project/client/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <!-- <nav>
+    <router-link to="/">사원목록</router-link> |
+    <router-link to="/empInsert">사원등록</router-link> |
+  </nav> -->
+  <div>
+    <Header />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+<script>
+import Header from "./layouts/Header.vue";
+import Footer from "./layouts/FooterComponent.vue";
+export default {
+  components: {
+    Header: Header,
+    Footer: Footer,
+  },
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ emp-project/client/src/layouts/Header.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <ul class="nav nav-underline">
+    <li class="nav-item">
+      <router-link class="nav-link active" to="/">nodeAndVue</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link class="nav-link" to="/">사원조회</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link class="nav-link" to="/empInsert">사원등록</router-link>
+    </li>
+  </ul>
+</template>

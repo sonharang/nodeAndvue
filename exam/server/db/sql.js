2ca8f82a2e10e1530a23d0a7439c782c8f8d34ea
하랑 <148874659+sonharang@users.noreply.github.com>
2023-12-08 10:01:19 +0900

    Delete exam/server directory

--- exam/server/db/sql.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  conList: `select t_board_board.no,t_board_board.title,t_board_board.writer,t_board_board.content,t_board_board.created_date,t_comment_board.bno from t_board_board left join t_comment_board  on t_board_board.no = t_comment_board.bno;`,
-  conInfo: `select t_board_board.no,t_board_board.title,t_board_board.writer,t_board_board.content,t_board_board.created_date,t_comment_board.bno from t_board_board left join t_comment_board  on t_board_board.no = t_comment_board.bno where t_board_board.no = ?;`,
-  conInsert: `insert into t_board_board set ? `,
-  conUpdate: `update t_board_board set ? where no = ?`,
-};
